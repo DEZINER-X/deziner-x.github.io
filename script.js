b0cf81e119ea45667c00b61569b897aabd3d13d4
DEZINER-X <deziner.x101@gmail.com>
2025-03-02 02:39:40 +0530

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     setTimeout(() => {
         // Move the intro to the top
         document.getElementById("intro").classList.add("hidden");
-
+    }, 500);
         // Show the content smoothly
         setTimeout(() => {
             document.getElementById("content").style.opacity = "1";
